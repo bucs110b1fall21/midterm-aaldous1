a21d66b22186c067ce1cdf8cd145414009baf954
Andrew Aldous <aaldous1@binghamton.edu>
2021-10-22 11:38:45 -0400

    Commit 2 - functions

--- main.py
+++ main.py
@@ -6,6 +6,35 @@ startX = -100
 startY = 20
 raceOne = 100
 raceTwo = 10
+
+def returnTurtles(turtle1, turtle2):
+	"""
+	Returns turtles to respective starting positions
+	args: 2 turtle objects
+	return: none
+	"""
+	turtle1.goto(startX,startY)
+	turtle2.goto(startX,-startY)
+def raceTurtles(turtle1, turtle2, value):
+	"""
+	Brings turtles forward by a random interval determined by passed value
+	args: 2 turtle objects, int
+	return: none
+	"""
+	turtle1.forward(random.randrange(1,value+1)) 
+	turtle2.forward(random.randrange(1,value+1))
+def drawPolygon(turtle, length, sides):
+	"""
+	Uses a turtle object to draw an n-sided polygon with specified length
+	args: turtle object, 2 ints
+	return: string
+	"""
+	for i in range(sides):
+		turtle.forward(length)
+		turtle.left(360/sides)
+	turtle.clear()
+	return print("Polygon with " + str(sides) + " sides")
+	
 def main():
 	#Part A
 	window = turtle.Screen()       # 2.  Create a screen
@@ -20,29 +49,21 @@ def main():
 
 	michelangelo.up()          # 4.  Pick up the pen so we don’t get lines
 	leonardo.up()
-	michelangelo.goto(startX,startY)
-	leonardo.goto(startX,-startY)
+	returnTurtles(michelangelo, leonardo)
 
 	## 5. your code goes here
-	michelangelo.forward(random.randrange(1,raceOne+1)) 
-	leonardo.forward(random.randrange(1,raceOne+1))
-	michelangelo.goto(startX,startY)
-	leonardo.goto(startX,-startY)
+	raceTurtles(michelangelo, leonardo, raceOne)
+	returnTurtles(michelangelo, leonardo)
 
 	for i in range(raceTwo):
-		michelangelo.forward(random.randrange(0,raceTwo+1))
-		leonardo.forward(random.randrange(0,raceTwo+1))
-	michelangelo.goto(startX,startY)
-	leonardo.goto(startX,-startY)
+		raceTurtles(michelangelo, leonardo, raceTwo)
+	returnTurtles(michelangelo, leonardo)
 
 	# Part B - complete part B here
 	A = [3,4,6,9,12]
 	michelangelo.down()
 	for i in A:
-		for j in range(i):
-			michelangelo.forward(lengthPolygon)
-			michelangelo.left(360/i)
-		michelangelo.clear()
-	
+		drawPolygon(michelangelo, lengthPolygon, i)
+		
 	window.exitonclick()
 main()
